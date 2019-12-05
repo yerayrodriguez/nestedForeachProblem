9dc69d7f0086cc3562907952d3988410732127d6
Yeray Rodriguez <yrodriguez@opencanarias.es>
2019-12-05 10:04:53 +0000

    Ajax and non ajax request added.

--- src/main/java/com/yerayrodriguez/nestedForeachProblem/TestManager.java
+++ src/main/java/com/yerayrodriguez/nestedForeachProblem/TestManager.java
@@ -23,23 +23,29 @@ public class TestManager implements Serializable {
     // Parent 0 with no children
     Parent parent0 = new Parent("P0");
     root.add(parent0);
-    // Parent 1 with 3 children
+    // Parent 1 with 2 children
     Parent parent1 = new Parent("P1");
     parent1.getChildren().add(new Child("C0"));
     parent1.getChildren().add(new Child("C1"));
-    parent1.getChildren().add(new Child("C2"));
     root.add(parent1);
   }
 
-  public void extractFirst() {
- // If P1 has no children, return
-    if (root.get(1).getChildren().isEmpty()) {
-      return;
+  public String submit() {
+    Parent P0 = root.get(0);
+    Parent P1 = root.get(1);
+    if (!P0.getChildren().isEmpty()) {
+      return null;
     }
     // Get first child of P1
-    Child removedChild = root.get(1).getChildren().remove(0);
+    Child removedChild = P1.getChildren().remove(0);
+    System.out.println("Removed child from P1: " + removedChild.getId()); // OK
+    System.out.println("Is removed child id equals 'C0': " + removedChild.getId().equals("C0")); // OK
     // Add this child to P0
-    root.get(0).getChildren().add(removedChild);
+    P0.getChildren().add(removedChild);
+    Child firstP0Child = P0.getChildren().get(0);
+    System.out.println("Frist P0 Child: " + firstP0Child.getId()); // OK
+    System.out.println("Is first P0 child id equals 'C0': " + firstP0Child.getId().equals("C0")); // OK
+    return null;
   }
 
 }
